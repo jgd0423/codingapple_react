59b183dd7bb0e9e2ee880aad4c81225ee305c581
Jeong Geumdam <>
2023-05-01 15:35:06 +0900

    중요한 데이터는 변수말고 state에 담습니다

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  let post = '강남 우동 맛집';
+  let [title, setTitle] = useState('남자 코트 추천');
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>블로그임</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{title}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
