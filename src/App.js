012a16eb73f6514bf35fe51aa06d70723bffa410
Jeong Geumdam <>
2023-05-01 16:32:15 +0900

    map : 많은 div들을 반복문으로 줄이고 싶은 충동이 들 때

--- src/App.js
+++ src/App.js
@@ -2,44 +2,37 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
-  let [thumbsUp, setThumbsUp] = useState(0);
+  let [titles, setTitles] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [thumbsUp, setThumbsUp] = useState([0, 0, 0]);
   let [isModalShow, setIsModalShow] = useState(false);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>ReactBlo</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <div className="list">
-        <h4>
-          {title[0]}{' '}
-          <span
-            onClick={() => {
-              setThumbsUp(++thumbsUp);
-            }}
-          >
-            👍
-          </span>{' '}
-          {thumbsUp}{' '}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4
-          onClick={() => {
-            setIsModalShow(!isModalShow);
-          }}
-        >
-          {title[2]}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-
+      {titles.map((title, idx) => {
+        return (
+          <div className="list" key={idx}>
+            <h4>
+              {title}{' '}
+              <span
+                onClick={() => {
+                  const thumbsUpCopy = [...thumbsUp];
+                  let clickedThumbsUp = thumbsUpCopy[idx];
+                  clickedThumbsUp++;
+                  thumbsUpCopy[idx] = clickedThumbsUp;
+                  setThumbsUp(thumbsUpCopy);
+                }}
+              >
+                👍
+              </span>{' '}
+              {thumbsUp[idx]}
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
       {isModalShow ? <Modal /> : null}
     </div>
   );
