5cb2aee93d834ff25bcc95c5d8c91b9506ea6d3a
Jeong Geumdam <>
2023-05-15 22:49:50 +0900

    상세보기가 열려있는 상태에서 다른 글을 눌렀을 때 글이 닫히지 않게 하는 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   let [titles, setTitles] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
   let [title, setTitle] = useState(titles[0]);
   let [thumbsUps, setThumbsUps] = useState([0, 0, 0]);
   let [isModalShow, setIsModalShow] = useState(false);
+  let [inputValue, setInputValue] = useState('');
+  let [titleIndex, setTitleIndex] = useState({ prev: null, now: null });
+
+  useEffect(() => {
+    if (titleIndex.prev === null && titleIndex.now === null) {
+      return;
+    }
+
+    console.log(`prevTitleIndex: ${titleIndex.prev}`);
+    console.log(`nowTitleIndex: ${titleIndex.now}`);
+
+    if (titleIndex.prev !== titleIndex.now) {
+      setIsModalShow(true);
+    }
+
+    if (titleIndex.prev === titleIndex.now) {
+      setIsModalShow(false);
+      setTitleIndex({ prev: null, now: null });
+    }
+  }, [titleIndex]);
 
   return (
     <div className="App">
@@ -18,12 +38,13 @@ function App() {
             <h4
               onClick={() => {
                 setTitle(title);
-                setIsModalShow(!isModalShow);
+                setTitleIndex({ prev: titleIndex.now, now: idx });
               }}
             >
               {title}{' '}
               <span
-                onClick={() => {
+                onClick={(e) => {
+                  e.stopPropagation();
                   const thumbsUpsCopy = [...thumbsUps];
                   let clickedThumbsUp = thumbsUpsCopy[idx];
                   clickedThumbsUp++;
@@ -36,9 +57,30 @@ function App() {
               {thumbsUps[idx]}
             </h4>
             <p>2월 17일 발행</p>
+            <button
+              onClick={() => {
+                const removedTitles = [...titles.slice(0, idx), ...titles.slice(idx + 1)];
+                setTitles(removedTitles);
+              }}
+            >
+              삭제
+            </button>
           </div>
         );
       })}
+
+      <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+      <button
+        onClick={() => {
+          const titlesCopy = [...titles];
+          titlesCopy.unshift(inputValue);
+          setTitles(titlesCopy);
+          setInputValue('');
+        }}
+      >
+        글추가
+      </button>
+
       {isModalShow ? <Modal title={title} /> : null}
     </div>
   );
